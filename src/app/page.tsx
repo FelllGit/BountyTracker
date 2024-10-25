ad07ed141cc3351f9dec61e1294416f689921b85
Danylo Paliienko <it18@eurocommerce.biz>
2024-10-25 11:25:20 +0300

    added metadatas for pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,24 @@ import "./app.css";
 
 import CrowdsourcedAudits from "@/components/home/CrowdSourcedAudits/CrowdsourcedAudits";
 import CrowdsourcedAuditsTable from "@/components/home/CrowdSourcedAudits/CrowdsourcedAuditsTable";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-8">
-      <CrowdsourcedAudits />
-      <CrowdsourcedAuditsTable />
-    </div>
+    <>
+      <Head>
+        <title>Home - Crowdsourced Audits</title>
+        <meta
+          name="description"
+          content="Welcome to the Crowdsourced Audits platform."
+        />
+        <meta name="keywords" content="audits, crowdsourcing, home" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div className="flex flex-col gap-8">
+        <CrowdsourcedAudits />
+        <CrowdsourcedAuditsTable />
+      </div>
+    </>
   );
 }
