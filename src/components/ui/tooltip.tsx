f15bfb6d11ea095880c883f260a91638d161732e
Danylo Paliienko <it18@eurocommerce.biz>
2025-02-21 12:20:46 +0200

    improved tooltip for mobile devices

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,78 +1,32 @@
 "use client";
 
-import { forwardRef, useState, type ReactNode } from "react";
+import * as React from "react";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
 import { cn } from "@/lib/utils";
 
 const TooltipProvider = TooltipPrimitive.Provider;
 
-interface TooltipProps {
-  children: ReactNode;
-  defaultOpen?: boolean;
-}
+const Tooltip = TooltipPrimitive.Root;
 
-const Tooltip = ({
-  children,
-  defaultOpen = false,
-  ...props
-}: TooltipProps & TooltipPrimitive.TooltipProps) => {
-  const [isOpen, setIsOpen] = useState(defaultOpen);
+const TooltipTrigger = TooltipPrimitive.Trigger;
 
-  return (
-    <TooltipPrimitive.Root open={isOpen} onOpenChange={setIsOpen} {...props}>
-      {children}
-    </TooltipPrimitive.Root>
-  );
-};
-
-interface TooltipTriggerProps
-  extends Omit<
-    React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Trigger>,
-    "onClick" | "onToggle" | "align"
-  > {
-  onClick?: (event: React.MouseEvent<HTMLDivElement>) => void;
-}
-
-const TooltipTrigger = forwardRef<HTMLDivElement, TooltipTriggerProps>(
-  ({ onClick, ...props }, ref) => {
-    const [isClicked, setIsClicked] = useState<boolean>(false);
-
-    const handleClick = (event: React.MouseEvent<HTMLDivElement>): void => {
-      setIsClicked(!isClicked);
-      onClick?.(event);
-    };
-
-    return (
-      <TooltipPrimitive.Trigger asChild {...props}>
-        <div ref={ref} onClick={handleClick} />
-      </TooltipPrimitive.Trigger>
-    );
-  }
-);
-TooltipTrigger.displayName = TooltipPrimitive.Trigger.displayName;
-
-interface TooltipContentProps
-  extends React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content> {
-  className?: string;
-  sideOffset?: number;
-}
-
-const TooltipContent = forwardRef<HTMLDivElement, TooltipContentProps>(
-  ({ className, sideOffset = 4, ...props }, ref) => (
-    <TooltipPrimitive.Portal>
-      <TooltipPrimitive.Content
-        ref={ref}
-        sideOffset={sideOffset}
-        className={cn(
-          "z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-          className
-        )}
-        {...props}
-      />
-    </TooltipPrimitive.Portal>
-  )
-);
+const TooltipContent = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, sideOffset = 4, ...props }, ref) => (
+  <TooltipPrimitive.Portal>
+    <TooltipPrimitive.Content
+      ref={ref}
+      sideOffset={sideOffset}
+      className={cn(
+        "z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        className
+      )}
+      {...props}
+    />
+  </TooltipPrimitive.Portal>
+));
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
 export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
