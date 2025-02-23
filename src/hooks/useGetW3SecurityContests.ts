77a989a8a113572db3fd75980221f027acc2747e
Danylo Paliienko <it18@eurocommerce.biz>
2025-02-23 09:53:05 +0200

    fixed Pectra

--- src/hooks/useGetW3SecurityContests.ts
+++ src/hooks/useGetW3SecurityContests.ts
@@ -57,7 +57,72 @@ const fetchW3SecurityContests = async (
     );
   }
 
-  return response.json();
+  const data = await response.json();
+
+  const filteredContests = data.filter(
+    (contest: CrowdsourcedAudit) =>
+      ![
+        "lighthouse",
+        "besu",
+        "teku",
+        "grandine",
+        "reth",
+        "Ethereum-Protocol",
+        "nimbus-eth2",
+        "prysm",
+        "nethermind",
+        "lodestar",
+        "erigon",
+        "go-ethereum",
+      ].some(
+        (name) =>
+          contest.project.includes(name) && contest.platform === "Cantina"
+      )
+  );
+
+  const hasEthereumContests = data.some((contest: CrowdsourcedAudit) =>
+    [
+      "lighthouse",
+      "besu",
+      "teku",
+      "grandine",
+      "reth",
+      "Ethereum-Protocol",
+      "nimbus-eth2",
+      "prysm",
+      "nethermind",
+      "lodestar",
+      "erigon",
+      "go-ethereum",
+    ].some(
+      (name) => contest.project.includes(name) && contest.platform === "Cantina"
+    )
+  );
+
+  if (hasEthereumContests) {
+    filteredContests.push({
+      id: "pectra",
+      project: "Ethereum Foundation / Pectra",
+      slug: "pectra",
+      platform: "Cantina",
+      originalUrl: "https://cantina.xyz/competitions/pectra",
+      imageUrl:
+        "https://imagedelivery.net/wtv4_V7VzVsxpAFaxzmpbw/8d6965f0-dd45-4838-2799-68bee7693600/public",
+      maxReward: 2000000,
+      languages: [],
+      rewardsPool: 2000000,
+      rewardsToken: "USDC",
+      startDate: "2025-02-21T20:00:00.000Z",
+      endDate: "2025-03-24T20:00:00.000Z",
+      evaluationEndDate: "2025-03-24T20:00:00.000Z",
+      status: "ONGOING",
+      tags: ["#contest"],
+      likes: [],
+      dislikes: [],
+    });
+  }
+
+  return filteredContests;
 };
 
 export const useGetW3SecurityContests = (
@@ -71,7 +136,6 @@ export const useGetW3SecurityContests = (
   excludeLanguages?: boolean
 ) => {
   return useQuery({
-    // eslint-disable-next-line no-restricted-syntax
     queryKey: [
       "w3-security-contests",
       search,
